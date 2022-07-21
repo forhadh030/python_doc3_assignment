7720ee1af29a3941782bf11f3309de6d1192ea0f
Forhad Hussain <forhad@ymail.com>
2022-07-21 14:18:08 -0700

    first commit

--- Area.py
+++
@@ -1,5 +0,0 @@
-def house(width, height):
-
-    area = width * height
-    return f"Based on your response, the square footage of your house is: {area}"
-
--- Circumference.py
+++
@@ -1,5 +0,0 @@
-import math
-
-def circle(radius):
-    circumference = 2 * 3.14159265359 * radius
-    return f"Based on your response, the circumference of your circle is: {circumference}"
